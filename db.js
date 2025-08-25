3ba2cbdac922e6c763374d92f36e17a1cd9ecab6
halil gezer <hg_1993@windowslive.com>
2025-08-26 00:22:38 +0300

    gitignore silindi env açıldı

--- db.js
+++ db.js
@@ -2,11 +2,11 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: process.env.DB_HOST || '213.142.148.67',
+  port: process.env.DB_PORT || '5432',
+  user: process.env.DB_USER || 'postgres',
+  password: process.env.DB_PASSWORD || 'Rtq6In4ZxA2U7aj5GCz7dg3spwY4smraqMcB4ZlXH4t72zz2E9zXxyO6cVoTu0mb',
+  database: process.env.DB_NAME || 'hcasoft' ,
 });
 
 module.exports = pool;
